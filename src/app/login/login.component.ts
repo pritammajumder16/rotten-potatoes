c4ad9bb5a4953447bc23572086e844cc31f01311
pritam16 <pritammajumder416@gmail.com>
2024-09-24 09:32:53 +0530

    login Form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatButtonModule, MatFormFieldModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  login(form: NgForm) {
+    console.log(form.value);
+    if (!form.valid) return;
+  }
 }
