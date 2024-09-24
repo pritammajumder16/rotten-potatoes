c8b2204616aa9237512b9f8479c99de83013a6c5
pritam16 <pritammajumder416@gmail.com>
2024-09-24 08:04:22 +0530

    Initial routes and parent styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movie',
+    component: MovieComponent,
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+];
