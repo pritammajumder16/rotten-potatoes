c7268d6c7ff74cca10eb3f363c4c4341bf257da5
pritam16 <pritammajumder416@gmail.com>
2024-09-24 08:02:25 +0530

    Fisrt commit

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.scss'
+})
+export class MovieComponent {
+
+}
